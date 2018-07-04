7c3128461904404185ef0faffd0c307a69c6080d
Tor Røkkum Børset <torrokku@gmail.com>
2018-07-04 23:49:47 +0200

    Lastet opp mulighet for å oppdatere data

--- kommune.py
+++ kommune.py
@@ -7,8 +7,19 @@ class Kommune(Base):
 
     ogc_fid = Column(Integer, primary_key=True)
     navn = Column(String)
+    fylke = Column(String)
+    kommunenummer = Column(String)
+    kommunenavn = Column(String)
+    fylkesnavn = Column(String)
 
-    def __init__(self, navn):
+
+
+    def __init__(self,ogc_fid, navn, fylke, kommunenummer, kommunenavn, fylkesnavn):
+        self.ogc_fid = ogc_fid
         self.navn = navn
+        self.fylke = fylke
+        self.kommunenummer = kommunenummer
+        self.kommunenavn = kommunenavn
+        self.fylkesnavn = fylkesnavn
 
 
--- test.py
+++ test.py
@@ -2,11 +2,34 @@
 
 from base import Session, engine, Base
 from kommune import Kommune
+from json import loads
 
 session = Session()
 
-kommune = session.query(Kommune).first()
+kommune = session.query(Kommune).all()
+##test = session.query(Kommune).filter(Kommune.navn == 'Testkommune').\
+##    delete(synchronize_session=False)
+##session.commit()
+test =  [[x.ogc_fid ,loads(x.navn), loads(x.fylke)] for x in kommune]
+
+##print test
+##
+##for x in test:
+##    try:
+##        print x[0]['navn']
+##    except:
+##        print x
+
+##liste = [(x[0], x[1][0]['navn'], x[2][0]['navn']) for x in test]
+##
+##for x in liste:
+##    item = session.query(Kommune).filter(Kommune.ogc_fid == x[0]).one()
+##    item.fylkesnavn = x[2]
+##    item.kommunenavn = x[1]
+##    session.add(item)
+
+##item = session.query(Kommune).filter(Kommune.ogc_fid == 54).one()
 
 
-
+##session.commit()
 
